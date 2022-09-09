14fd5cb8db7adff749f78cee0c964a622a23bf3d
inulka82@mail.ru <inulka82@mail.ru>
2022-09-09 21:50:08 +0300

    M16_Task_09092022

--- M16_Task1/Program.cs
+++ M16_Task1/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using Garden_Library;
 
 namespace M16_Task1
